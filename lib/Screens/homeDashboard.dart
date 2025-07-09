1abdff13c65251a9758f60615d71d084041d3e26
IAftabRehman <iamaftabrehman@gmail.com>
2025-07-09 22:56:15 +0500

    Some extra changes in also create
    homeDashboard.dart screen

--- lib/Screens/homeDashboard.dart
+++ lib/Screens/homeDashboard.dart
@@ -1,12 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class HomeDashboard extends StatelessWidget {
+class HomeDashboard extends StatefulWidget {
   const HomeDashboard({super.key});
+
+  @override
+  State<HomeDashboard> createState() => _HomeDashboardState();
+}
+
+class _HomeDashboardState extends State<HomeDashboard> {
+  int selectedIndex = 0;
+
+  List<Widget> screenList = [Text("First"), Text("First"), Text("First"), Text("First"), Text("First"),];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Dashboard!", style: GoogleFonts.raleway(fontSize: 23.04, fontWeight: FontWeight.bold)),
+              RichText(
+                text: TextSpan(
+                  text: 'Welcome to Dashboard, ',
+                  style: GoogleFonts.raleway(color: Colors.black, fontSize: 11.11, fontWeight: FontWeight.w400),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'Aftab',
+                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
 
+              SizedBox(height: 30),
+
+              Row(
+                children: [
+                  Expanded(child: ReUsableContainer(icon: Icons.question_mark, upperText: "224", lowerText: "Total Questions")),
+                  SizedBox(width: 15),
+                  Expanded(child: ReUsableContainer(icon: Icons.done_all, upperText: "154", lowerText: "Answer\nQuestions")),
+                ],
+              ),
+              SizedBox(height: 15),
+              Row(
+                children: [
+                  Expanded(child: ReUsableContainer(icon: Icons.panorama_fish_eye_outlined, upperText: "1.5k", lowerText: "Total Views")),
+                  SizedBox(width: 15),
+                  Expanded(child: ReUsableContainer(icon: Icons.youtube_searched_for_rounded, upperText: "12", lowerText: "Total Videos")),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (val) {
+            selectedIndex = val;
+            setState(() {});
+          },
+          backgroundColor: Colors.red,
+          currentIndex: selectedIndex,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home, size: 40), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.question_mark_outlined, size: 40), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.video_call, size: 40), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.comment, size: 40), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.person, size: 40), label: ""),
+          ]),
     );
   }
 }
+
+
+class ReUsableContainer extends StatelessWidget {
+  final IconData icon;
+  final String upperText;
+  final String lowerText;
+  const ReUsableContainer({super.key, required this.icon, required this.upperText, required this.lowerText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Color(0xffFFFFFF),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 8,
+            spreadRadius: 0,
+            offset: Offset(0, 10),
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 40, color: Color(0xffD4D4D4)),
+          SizedBox(width: 10),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(upperText, style: GoogleFonts.raleway(fontSize: 23.04, fontWeight: FontWeight.bold)),
+              Text(lowerText, style: GoogleFonts.raleway(fontSize: 9.94, fontWeight: FontWeight.w400))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
